8a9012c508a1513590243343b30e0a5ac7e6f08e
TG49 <93449051+TG49@users.noreply.github.com>
2022-01-30 22:08:12 +0000

    Added plotly to dependencies and optimised map

--- Optional_Extensions.py
+++ Optional_Extensions.py
@@ -1,6 +1,5 @@
 from floodsystem.geo import displayStationLocation, stationObjectsByRiver
 from floodsystem.stationdata import build_station_list
-import plotly.graph_objects as go
 
 rivers = ["River Cam", "River Thames"]
 stations = build_station_list()
