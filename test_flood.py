021f6a40bd9a9ee1da3e07ad6fae3f73624dac3b
af765 <af765@cam.ac.uk>
2022-02-12 18:56:01 +0000

    Added Test for Task 2B

---
+++ test_flood.py
@@ -0,0 +1,15 @@
+from floodsystem.station import MonitoringStation
+from floodsystem.flood import stations_level_over_threshold
+from floodsystem.stationdata import build_station_list, update_water_levels
+
+a = MonitoringStation(label="a", latest_level = 7, station_id = 1, measure_id=1, coord=(1,1), river="A", town = "townA", typical_range=(0.5,10))
+b = MonitoringStation(label="b", latest_level = 7, station_id = 2, measure_id=2, coord=(4,4), river="A", town = "townB",typical_range=(0.2,10))
+c = MonitoringStation(label="c", latest_level = 7, station_id = 3, measure_id=3, coord=(3,3), river="B", town = "townC", typical_range=(0,5))
+stations = [a,b,c]
+
+def test_stations_level_over_threshold():
+    stations_by_tol = stations_level_over_threshold(stations, 0.7)
+    assert stations_by_tol[0][0] == c
+
+if __name__ == "__main__":
+    test_stations_level_over_threshold()
