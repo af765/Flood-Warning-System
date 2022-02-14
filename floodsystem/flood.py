37e7ffe494188c3937bcc359d48c132175e16cad
TG49 <93449051+TG49@users.noreply.github.com>
2022-02-14 17:03:28 +0000

    Filter incorrect station data

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -16,9 +16,16 @@ def stations_highest_rel_level(stations, N):
     """Returns the stations with the N highest relative water levels"""
     stations_high = []
     for station in stations:
-        if (station.relative_water_level() != None):
+        if ((station.relative_water_level() != None) and (station.relative_water_level() < 200) ): #Add filter to remove stations with clearly incorrect data
             stations_high.append((station, station.relative_water_level()))
-
+        else:
+            if station.relative_water_level() == None:
+                reason = "No input data"
+                #print("WARNING: Station \"{}\" removed from consideration due to invalid input data ({})".format(station.name, reason))
+            else:
+                reason = "Error due to extreme relative water level (>200)"
+                print("WARNING: Station \"{}\" removed from consideration due to invalid input data ({})".format(station.name, reason))
+    
     stations_high = sorted_by_key(stations_high, 1)
     stations_high.reverse()
     return stations_high[:N]
