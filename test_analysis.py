1d02987a11568f19e8f731caf7402b4bea0070d9
TG49 <93449051+TG49@users.noreply.github.com>
2022-02-15 11:35:03 +0000

    Test 2F

---
+++ test_analysis.py
@@ -0,0 +1,27 @@
+from floodsystem.datafetcher import fetch_measure_levels
+import datetime
+from floodsystem.analysis import polyfit
+from floodsystem.stationdata import build_station_list, update_water_levels
+import numpy as np
+#As require historic data cannot create test data set. Therefore assert the function works by ensuring the output is not None. 
+
+def test_polyfit():
+     # Build list of stations
+    stations = build_station_list()
+
+    # Find station 'Cam'
+    for station in stations:
+        if station.name == 'Cam':
+            station_cam = station
+            break
+
+    # Fetch data over past 2 days
+    dt = 2
+    dates2, levels2 = fetch_measure_levels(
+        station_cam.measure_id, dt=datetime.timedelta(days=dt))
+
+    poly = polyfit(dates2, levels2, 4)
+    assert poly != None
+    assert type(poly) == tuple
+    assert type(poly[0]) == np.poly1d #ensure the elements of the tuple are correct type
+    assert type(poly[1]) == np.float64
